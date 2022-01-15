19ac6b200b1f03b6ef2801a0f90be7873cc6fc2a
jhhkim <94815029+jhhkim@users.noreply.github.com>
2022-01-15 15:32:27 +0900

    .

---
+++ irang_maptest/irang_maptest/src/main/java/com/project/irang_map/domain/user/Member.java
@@ -0,0 +1,49 @@
+package com.project.irang_map.domain.user;
+
+
+import com.project.irang_map.domain.Timestamped;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter // get 함수를 일괄적으로 만들어줍니다.
+@NoArgsConstructor // 기본 생성자를 만들어줍니다.
+@Entity // DB 테이블 역할을 합니다.
+public class Member extends Timestamped {
+
+    // ID가 자동으로 생성 및 증가합니다.
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    // 반드시 값을 가지도록 합니다.
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    // 사용자의 역할(회원, 관리자)
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+//    @Builder
+    public Member(String password, String email, Role role) {
+        
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
