a706b0e5c4cccc4eca930f8a9531538dc74006e3
jhhkim <aekim7@naver.com>
2022-01-15 09:09:55 +0900

    test

---
+++ src/main/java/com/project/irang_map/domain/KidsmapRepository.java
@@ -0,0 +1,12 @@
+package com.project.irang_map.domain;
+
+import java.util.List;
+
+import com.project.irang_map.dto.KidsmapDto;
+import com.project.irang_map.dto.KidsmapListResponseDto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KidsmapRepository extends JpaRepository<Kidsmap, Long> {
+    List<Kidsmap> findByAddrLike(String addr);
+}
---
+++ src/main/java/com/project/irang_map/service/KidsmapService.java
@@ -0,0 +1,40 @@
+package com.project.irang_map.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.project.irang_map.domain.Kidsmap;
+import com.project.irang_map.domain.KidsmapRepository;
+import com.project.irang_map.dto.KidsmapDto;
+import com.project.irang_map.dto.KidsmapListResponseDto;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class KidsmapService {
+    private final KidsmapRepository kidsmapRepository;
+
+    @Transactional(readOnly = true)
+    public List<KidsmapListResponseDto> findAllByAddr(String juso) {
+        return kidsmapRepository.findByAddrLike(juso).stream()
+                .map(KidsmapListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<KidsmapListResponseDto> findAll() {
+        return kidsmapRepository.findAll().stream()
+        .map(KidsmapListResponseDto::new)
+        .collect(Collectors.toList());
+    }
+
+    
+
+    
+
+}
