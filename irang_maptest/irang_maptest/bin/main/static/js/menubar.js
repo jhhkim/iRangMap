baa778a12be0426489ef13db951a172b006dea6d
jhhkim <94815029+jhhkim@users.noreply.github.com>
2022-01-15 15:33:00 +0900

    Delete irang_maptest/irang_maptest directory

--- irang_maptest/irang_maptest/bin/main/static/js/menubar.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-    document.getElementById("mySidepanel").style.height = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
