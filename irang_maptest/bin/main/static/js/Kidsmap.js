adc7dfcbf890a67712d317cee239a2d19806e8a7
jhhkim <94815029+jhhkim@users.noreply.github.com>
2022-01-17 00:17:35 +0900

    검색 결과만 마커로 표시, 위치보기 아이콘 클릭하면 해당 위치로 이동 새 마커 보여줌
    기존 마커 위에 새 마커를 얹는 형식
    개선할 점
    위치보기 아이콘을 클릭했을 때 기존 마커를 새 마커로 교체
    다른 행의 위치보기 아이콘을 클릭했을 때 이전의 새 마커는 사라지게
    같은 위치보기 아이콘을 여러번 클릭했을 때 이미지 중첩 되지 않게

--- irang_maptest/bin/main/static/js/Kidsmap.js
+++ irang_maptest/bin/main/static/js/Kidsmap.js
@@ -33,14 +33,7 @@
 //                         '</tr>';
 //                         $("#kidsmaplist").append(str);
 //            }
-            
-           
-           
-           
-
 //         }
-//document.write("<script src='b.js'></script>");
-
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 mapOption = {
@@ -52,32 +45,48 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 var bounds = new kakao.maps.LatLngBounds();
 
 
-//검색한 뒤에 나오게하고싶었는데 마커가 떴다가 초기위치로 지도가 돌아감
-// $("#inputbtn").click(function(){
-//첫화면에 바로 떠있음
 $("#kidsmaplist").ready(function(){
-    setMarker(address);
+    setMarker();
 });
 
 //마커찍기
-var geocoder = new kakao.maps.services.Geocoder();//service라이브러리 추가했는데 Geocoder인식안됨
-//address배열에 직접 주소 입력하는 대신 검색결과로 나온 주소를 넣어주면 됨
-var address=['부산시 강서구 범방동 1901-10',
-'부산시 강서구 신호동 262-9',
-'부산시 강서구 녹산동 97-5',
-'부산시 강서구 명지동 3461-17',
-'부산시 강서구 명지동 3365-7',
-'부산시 강서구 명지동 3442-2',
-'부산시 강서구 명지동 3238-15',
-'부산시 강서구 대저2동 1060-2',
-'부산시 강서구 명지동 3261-10',
-'부산시 강서구 대저1동 1459-7',
-'부산시 강서구 명지동 3363-3',
-'부산시 강서구 명지동 3357-6',
-'부산시 강서구 명지동 3395-4']
+//주소-좌표 변환 객체 생성
+var geocoder = new kakao.maps.services.Geocoder();
+//service라이브러리 추가했는데 Geocoder인식안됨(&libraries=services 붙어있는 링크만 apikey값넣어서 스크립트src에 넣고 나머지 삭제하니까 해결)
 
-function setMarker(address){
-    
+//검색결과로 나온 주소들 배열에 넣는 함수
+// function getAddress(){
+//     var address = [];
+//     var rows = document.getElementById("kidsmaplist").getElementsByTagName("tr");//tbody행 개수 구하기
+//     for (i = 0; i < rows.length; i++) {
+//         var adr = document.getElementById("kidsmaplist").getElementsByTagName("tr")[i].getElementsByTagName("td")[1].innerHTML;//addr이 있는 두번째 열 값 가져옴
+//         address.push(adr);//address 배열에 넣음
+//     }
+// }
+
+//address배열에 직접 주소 입력하는 대신 검색결과로 나온 주소를 넣어주면 됨
+// var address=['부산시 강서구 범방동 1901-10',
+// '부산시 강서구 신호동 262-9',
+// '부산시 강서구 녹산동 97-5',
+// '부산시 강서구 명지동 3461-17',
+// '부산시 강서구 명지동 3365-7',
+// '부산시 강서구 명지동 3442-2',
+// '부산시 강서구 명지동 3238-15',
+// '부산시 강서구 대저2동 1060-2',
+// '부산시 강서구 명지동 3261-10',
+// '부산시 강서구 대저1동 1459-7',
+// '부산시 강서구 명지동 3363-3',
+// '부산시 강서구 명지동 3357-6',
+// '부산시 강서구 명지동 3395-4']
+
+function setMarker(){
+    //address.length의 address undefined라고 떠서 getAddress함수 안 내용 그대로 넣음
+    var address = [];
+    var rows = document.getElementById("kidsmaplist").getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        var adr = document.getElementById("kidsmaplist").getElementsByTagName("tr")[i].getElementsByTagName("td")[1].innerHTML;
+        address.push(adr);
+    }
     //for문에 넣어서 좌표로 모두 변환
     for(var i = 0; i<address.length; i++){
                 
@@ -103,6 +112,61 @@ function setMarker(address){
             })
             }     
         }
+
+//위치보기 아이콘 클릭하면 지도에 마커 띄워주는 함수
+//클릭한 아이콘이 있는 행의 인덱스 번호 띄워줌
+// function showMarker(){
+// $('#kidsmaplist').find('tr').click( function(){
+//     //($(this).index());
+//     alert('You clicked index '+ ($(this).index()) );
+//     });
+// }
+
+function showMarker(){
+    $('#kidsmaplist').find('tr').click(function(){
+        //var tr = $(this);
+        var td = $(this).children();
+        var myPick = td.eq(1).text();//위치보기 아이콘 클릭하면 해당 행의 2번째열 내용 받음
+
+        geocoder.addressSearch(myPick, function(result, status){
+            //정상적으로 검색이 완료됐으면
+            if (status === kakao.maps.services.Status.OK){
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                // 결과값으로 받은 위치를 새 마커로 표시합니다(기존 마커 위에 얹음)
+                // 마커 이미지의 이미지 주소입니다
+                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+                // 마커 이미지의 이미지 크기 입니다
+                var imageSize = new kakao.maps.Size(28.36, 41.36);//original size(24,35) 1.5배(35.97, 52.46)
+                // 마커 이미지를 생성합니다    
+                var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+                //마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords,
+                image: markerImage
+                
+        });
+                 //레벨도 조정
+                // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
+                var bounds = new kakao.maps.LatLngBounds();
+                // LatLngBounds 객체에 좌표를 추가한다
+                 bounds.extend(coords);
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다(레벨변화로 줌은 안일어났음)
+                //map.setCenter(coords);
+                // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
+                // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
+                map.setBounds(bounds);
+                // //다른 행의 위치보기 아이콘 누르면 입힌 마커 없어지게, 같은 행의 아이콘 또 눌러도 마커 또 위에 생기지 않게하고싶음
+                // if (){
+                //     // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+                //     marker.setMap(null);   
+                // }
+            }
+            
+        })
+    })
+}
 
 // //마커찍기
 // var geocoder = new kakao.maps.services.Geocoder();
@@ -148,33 +212,3 @@ function setMarker(address){
 //         '</tr>';
 //         $("kidsmaplist").append(str);
 //             }
-//         //class=showStore인 아이콘을 클릭하면 가게가 위치한 곳으로 지도 이동
-//         $(".showStore").click(function(){
-        
-//         // 주소로 좌표를 검색합니다
-//         geocoder.addressSearch(data[i].addr, function(result, status) {
-    
-//             // 정상적으로 검색이 완료됐으면 
-//             if (status === kakao.maps.services.Status.OK) {
-    
-//                 var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-    
-//                 // 결과값으로 받은 위치를 마커로 표시합니다
-//                 var marker = new kakao.maps.Marker({
-//                     map: map,
-//                     position: coords
-//                 });
-    
-                
-//                 // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-//                 map.setCenter(coords);
-//             }
-        
-//     });
-//         });
-    
-    
-    
-// };
-//셀 클릭하면 해당가게가 있는 마커를 보여주는 함수
-
